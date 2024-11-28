838aa79f3f1535f79d67fc66a0c4020a13c47424
matteodragotto <matteodrag8@gmail.com>
2024-11-28 16:03:33 +0100

    [feat] css styling

--- js/script.js
+++ js/script.js
@@ -3,37 +3,37 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "assets/img/female3.png"
   }
 ];
 
@@ -42,20 +42,22 @@ const row = document.querySelector('.row')
 
 function newMemberCard (member) {
   const {name, role, email, img} = member
-  return           `<div class="card">
-                     <div class="img">
-                       <img src="${img}" alt="${img}">
-                     </div>
-                     <div class="name">
-                       <p>${name}</p>
-                     </div>
-                     <div class="role">                   
-                       <p>${role}</p>
-                     </div>
-                     <div class="email">
-                       <p>${email}</p>
-                     </div>
-                   </div>`
+  return  `<div class="card">
+            <div class="img">
+              <img src="${img}" alt="${img}">
+            </div>
+            <div class="txt">
+              <div class="name">
+                <p>${name}</p>
+              </div>
+              <div class="role">                   
+                <p>${role}</p>
+              </div>
+              <div class="email">
+                <p>${email}</p>
+              </div>
+            </div>            
+          </div>`
 }
 
 
